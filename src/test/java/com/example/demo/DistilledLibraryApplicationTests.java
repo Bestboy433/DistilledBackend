2f8e983609cb550819d960680aef7968b056de06
Steven Oyinlola <j.oyinlola95@gmail.com>
2022-04-22 22:50:31 +0100

    Tried to add tests but not enough time to do properly, just need to push changes and finalise assignment. All features seem to operate smoothly apart from updating status of book when a borrow is registered

--- src/test/java/com/example/demo/DistilledLibraryApplicationTests.java
+++ src/test/java/com/example/demo/DistilledLibraryApplicationTests.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.library.Author;
-import com.example.demo.library.AuthorRepository;
-import com.example.demo.library.Book;
-import com.example.demo.library.BookRepository;
+import com.example.demo.library.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,15 +30,16 @@ class DistilledLibraryApplicationTests {
 	//1. Add Book
 	private BookRepository book;
 	private AuthorRepository author;
+
+	private BorrowRepository borrow;
 	Author a1 = new Author("Arthur");
 	Book t1 = new Book(2637L,"Father Christmas", List.of(a1));
 	@Test
-	public Boolean tryAddBook() throws Exception {
-		int before = book.findAll().size();
-		this.mvc.perform(post("api/v1/library/addBook",t1));
+	public void tryAddBook() throws Exception {
+		int before = 0;
+		this.mvc.perform(post("/api/v1/library/addBook",t1));
 		int after = book.findAll().size();
-		if(before == after + 1) return true;
-		else return false;
+		assert(before == after - 1);
 
 	}
 
@@ -49,45 +47,51 @@ class DistilledLibraryApplicationTests {
 	Author a2 = new Author("Michael");
 
 	@Test
-	public Boolean tryAddAuthor() throws Exception {
-		int before = author.findAll().size();
-		this.mvc.perform(post("api/v1/library/addAuthor",a2));
+	public void tryAddAuthor() throws Exception {
+		int before = author.findAll() == null? 0 : author.findAll().size();
+		this.mvc.perform(post("/api/v1/library/addAuthor",a2));
 		int after = author.findAll().size();
-		if(before == after + 1) return true;
-		else return false;
+		assert(before == after + 1);
 
 	}
 
 	//3. Update Book Status
 	@Test
-	public Boolean tryUpdateBook() throws Exception {
-		this.mvc.perform(post("api/v1/library/addBook",t1));
-		this.mvc.perform(put("api/v1/library/updateStatus",t1));
-		if(book.findBookByIsbn(t1.getIsbn()).get(0).getStatus().matches("Borrowed")) return true;
-		else return false;
+	public void tryUpdateBook() throws Exception {
+		this.mvc.perform(post("http://localhost:8080/api/v1/library/addBook",t1));
+		this.mvc.perform(put("http://localhost:8080/api/v1/library/updateStatus",t1));
+		assert(book.findBookByIsbn(t1.getIsbn()).get(0).getStatus().matches("Borrowed"));
 	}
 	//4. Search Books in all 3 ways
 
 	//First by ISBN
 	@Test
 	public void trySearchIsbn() throws Exception {
-		this.mvc.perform(post("api/v1/library/addBook",t1));
-		this.mvc.perform(get("api/v1/library/book/isbn="+t1.getIsbn())).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString(t1.getTitle())));
+		this.mvc.perform(post("http://localhost:8080/api/v1/library/addBook",t1));
+		this.mvc.perform(get("http://localhost:8080/api/v1/library/book/isbn="+t1.getIsbn())).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString(t1.getTitle())));
 	}
 	//Second by Title
 	@Test
 	public void trySearchTitle() throws Exception {
-		this.mvc.perform(post("api/v1/library/addBook",t1));
-		this.mvc.perform(get("api/v1/library/book/title="+t1.getTitle())).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString(t1.getTitle())));
+		this.mvc.perform(post("http://localhost:8080/api/v1/library/addBook",t1));
+		this.mvc.perform(get("http://localhost:8080/api/v1/library/book/title="+t1.getTitle())).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString(t1.getTitle())));
 	}
 	//Third by Author
 	@Test
 	public void trySearchAuthor() throws Exception {
-		this.mvc.perform(post("api/v1/library/addBook",t1));
-		this.mvc.perform(get("api/v1/library/book/author="+t1.getAuthors().get(0).getName())).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString(t1.getTitle())));
+		this.mvc.perform(post("http://localhost:8080/api/v1/library/addBook",t1));
+		this.mvc.perform(get("http://localhost:8080/api/v1/library/author="+t1.getAuthors().get(0).getName())).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString(t1.getTitle())));
 	}
 
 	//5. Register a borrow of a book
+	Borrow b1 = new Borrow(t1,"bestboy");
+	@Test
+	public void tryBorrow() throws Exception {
+		int before = borrow.findAll().size();
+		this.mvc.perform(post("http://localhost:8080/api/v1/library/newBorrow",b1));
+		int after = borrow.findAll().size();
+		assert(before == after - 1);
+	}
 	@Test
 	void contextLoads() {
 	}
